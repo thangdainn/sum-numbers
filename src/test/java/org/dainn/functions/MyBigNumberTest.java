65fb72981b3f26fbb80b9c1993dbed5c49bcc67d
thann <thanngit@gmail.com>
2026-04-15 11:55:47 +0700

    feat: test func sum numbers

---
+++ src/test/java/org/dainn/functions/MyBigNumberTest.java
@@ -0,0 +1,27 @@
+package org.dainn.functions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyBigNumberTest {
+    private final MyBigNumber mbn =  new MyBigNumber();
+
+    @Test
+    void testBasicSum() {
+        assertEquals("2131", mbn.sum("1234", "897"));
+    }
+
+    @Test
+    void testWithCarry() {
+        assertEquals("1000", mbn.sum("999", "1"));
+        assertEquals("832", mbn.sum("154", "678"));
+    }
+
+    @Test
+    void testZero() {
+        assertEquals("123", mbn.sum("123", "0"));
+        assertEquals("456", mbn.sum("0", "456"));
+        assertEquals("123", mbn.sum("000123", "0"));
+    }
+}
